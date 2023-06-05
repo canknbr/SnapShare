f1bafa84981f4077757250cdcf9c2df8ec6bbb3a
canknbr <cankanbur1@gmail.com>
2023-06-05 18:11:37 +0300

    Initial Commit

---
+++ SnapShare/SnapShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapShareApp.swift
+//  SnapShare
+//
+//  Created by Can Kanbur on 5.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnapShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
